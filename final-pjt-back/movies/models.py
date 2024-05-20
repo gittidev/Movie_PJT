0b533ce9bb419848d4c077f1ec454aa859faf0f9
박보람 <qhfka961@gmail.com>
2024-05-20 11:31:36 +0900

    4차 병합

--- final-pjt-back/movies/models.py
+++ final-pjt-back/movies/models.py
@@ -7,21 +7,21 @@ class Movie(models.Model):
     like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_movies')
     adult = models.BooleanField()
     movie_id = models.IntegerField()
-    backdrop_path = models.TextField()
+    backdrop_path = models.TextField(null=True)
     genre_ids = models.TextField()
-    overview = models.TextField()
-    poster_path = models.TextField()
-    release_date = models.DateField()
+    overview = models.TextField(null=True)
+    poster_path = models.TextField(null=True)
+    release_date = models.TextField(null=True)
     original_title = models.CharField(max_length=100)
     title = models.CharField(max_length=100)
-    vote_average = models.FloatField()
+    vote_average = models.FloatField(null=True)
 
 class Community(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     title = models.CharField(max_length=30)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    create_user = models.ManyToManyField(settings.AUTH_USER_MODEL, symmetrical=False, related_name='myCommunity')
+    create_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_communities')
 
 
